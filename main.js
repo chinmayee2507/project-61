c13fd259c9c81040e8184af5f7dda15d205d9a71
chinmayee2507 <81042780+chinmayee2507@users.noreply.github.com>
2021-04-03 15:08:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+ function click_1(){
+    
+    location.replace("https://en.wikipedia.org/wiki/Bob_Ross")
+    
+}
+
+function click_2(){
+    
+    location.replace("https://goodnessinyou.com/about-us/")
+    
+}
